d44f19b326802a67cd27e34d56f42e572dd7c5fc
wenxpan <panwenxuan42@gmail.com>
2025-12-03 21:00:33 +1100

    302

--- day03/data/battery-joltage.test.ts
+++ day03/data/battery-joltage.test.ts
@@ -1,5 +1,10 @@
 import { expect, describe, it } from 'bun:test'
-import { calcTotalJoltage, checkBankJoltage } from './battery-joltage'
+import {
+  calcTotalJoltage,
+  calcTotalJoltageMoreDigits,
+  checkBankJoltage,
+  checkBankJoltageMuchMoreDigits,
+} from './battery-joltage'
 
 describe('checkBankJoltage', () => {
   it('finds the largest joltage possible', () => {
@@ -19,5 +24,26 @@ describe('calcTotalJoltage', () => {
     const input2 = await Bun.file('day03/data/02.txt').text()
     const banks2 = input2.trim().split('\n')
     expect(calcTotalJoltage(banks2)).toBe(17493)
+  })
+})
+
+describe('checkBankJoltageMuchMoreDigits', () => {
+  it('finds the largest joltage possible with more digits', () => {
+    expect(checkBankJoltageMuchMoreDigits('987654321111111')).toBe(987654321111)
+    expect(checkBankJoltageMuchMoreDigits('811111111111119')).toBe(811111111119)
+    expect(checkBankJoltageMuchMoreDigits('234234234234278')).toBe(434234234278)
+    expect(checkBankJoltageMuchMoreDigits('818181911112111')).toBe(888911112111)
+  })
+})
+
+describe('calcTotalJoltageMoreDigits', () => {
+  it('calcs total joltage with more digits', async () => {
+    const input = await Bun.file('day03/data/01.txt').text()
+    const banks = input.trim().split('\n')
+    expect(calcTotalJoltageMoreDigits(banks)).toBe(3121910778619)
+
+    const input2 = await Bun.file('day03/data/02.txt').text()
+    const banks2 = input2.trim().split('\n')
+    expect(calcTotalJoltageMoreDigits(banks2)).toBe(173685428989126)
   })
 })
--- day03/data/battery-joltage.ts
+++ day03/data/battery-joltage.ts
@@ -1,8 +1,9 @@
+const findLargestIndex = (numbers: number[]) => {
+  return numbers.findIndex((number) => number === Math.max(...numbers))
+}
+
 export const checkBankJoltage = (bank: string): number => {
   const numbers = bank.split('').map((char) => +char)
-  const findLargestIndex = (numbers: number[], isLast = false) => {
-    return numbers.findIndex((number) => number === Math.max(...numbers))
-  }
 
   const largestIndexRaw = findLargestIndex(numbers)
   const largestIndex =
@@ -19,6 +20,43 @@ export const calcTotalJoltage = (banks: string[]) => {
   let total = 0
   for (const bank of banks) {
     const number = checkBankJoltage(bank)
+    total += number
+  }
+  return total
+}
+
+export const checkBankJoltageMuchMoreDigits = (
+  bank: string,
+  digits = 12
+): number => {
+  const numbers = bank.split('').map((char) => +char)
+  const chosen = []
+
+  const findLargestIndexWithinRange = (
+    numbers: number[],
+    digitPlace: number,
+    startIndex = 0
+  ) => {
+    const range = numbers.slice(startIndex, numbers.length - digitPlace + 1)
+    const rangeIndex = findLargestIndex(range)
+
+    return rangeIndex + startIndex
+  }
+
+  let nextTimeStartIndex = 0
+  for (let i = digits; i > 0; i--) {
+    const result = findLargestIndexWithinRange(numbers, i, nextTimeStartIndex)
+    nextTimeStartIndex = result + 1
+    chosen.push(numbers[result])
+  }
+
+  return +chosen.join('')
+}
+
+export const calcTotalJoltageMoreDigits = (banks: string[]) => {
+  let total = 0
+  for (const bank of banks) {
+    const number = checkBankJoltageMuchMoreDigits(bank, 12)
     total += number
   }
   return total
