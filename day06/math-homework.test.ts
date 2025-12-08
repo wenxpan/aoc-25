497fa4ba29a2462d48c3b5810748a14ade42e863
wenxpan <panwenxuan42@gmail.com>
2025-12-08 20:33:38 +1100

    day06 X_X

---
+++ day06/math-homework.test.ts
@@ -0,0 +1,26 @@
+import { expect, describe, it } from 'bun:test'
+import { solveAdvancedWorksheet, solveWorksheet } from './math-homework'
+
+describe('solveWorksheet', () => {
+  it('solves problems for test input', async () => {
+    const input = await Bun.file('day06/data/01.txt').text()
+    expect(solveWorksheet(input)).toBe(4277556)
+  })
+
+  it('solves problems for full input', async () => {
+    const input = await Bun.file('day06/data/02.txt').text()
+    expect(solveWorksheet(input)).toBe(3525371263915)
+  })
+})
+
+describe('solveAdvancedWorksheet', () => {
+  it('solves problems for test input', async () => {
+    const input = await Bun.file('day06/data/01.txt').text()
+    expect(solveAdvancedWorksheet(input)).toBe(3263827)
+  })
+
+  it.only('solves problems for full input', async () => {
+    const input = await Bun.file('day06/data/02.txt').text()
+    expect(solveAdvancedWorksheet(input)).toBe(0)
+  })
+})
---
+++ day06/math-homework.ts
@@ -0,0 +1,85 @@
+const calcProblem = (input: number[], operation: string) => {
+  const answer = input.reduce(
+    (acc, currentVal) => {
+      switch (operation) {
+        case '*':
+          return acc * currentVal
+        case '+':
+          return acc + currentVal
+        default:
+          return 0
+      }
+    },
+    operation === '*' ? 1 : 0
+  )
+
+  return answer
+}
+
+export const solveWorksheet = (rawInput: string) => {
+  const lines = rawInput.trim().split('\n')
+  const rows = lines.map((line) => {
+    return line.split(' ').filter((val) => !!val)
+  })
+  const problemCount = rows[0]?.length!
+
+  let answer = 0
+  for (let i = 0; i < problemCount; i++) {
+    const problem = rows.map((row) => row[i])
+    const operation = problem[problem.length - 1]
+    const input = problem.slice(0, problem.length - 1).map((val) => +val!)
+    answer += calcProblem(input, operation!)
+  }
+
+  return answer
+}
+
+type Problem = {
+  operation: string
+  input: number[]
+}
+
+export const solveAdvancedWorksheet = (rawInput: string) => {
+  const lines = rawInput.split('\n')
+  const operationRow = lines[lines.length - 1]!
+
+  const operations = operationRow?.split(' ').filter((res) => !!res)
+
+  if (!operations) {
+    return 0
+  }
+
+  let problems: Problem[] = []
+  let lastIndex = 0
+  for (const [index, operation] of operations!.entries()) {
+    const startIndex =
+      operationRow.slice(lastIndex).indexOf(operation) + lastIndex
+    const endIndex = operations[index + 1]
+      ? operationRow.slice(startIndex + 1).indexOf(operations[index + 1]!) -
+        1 +
+        startIndex
+      : operationRow.length - 1
+
+    let input = []
+
+    for (let i = startIndex; i <= endIndex; i++) {
+      const num = lines
+        .slice(0, lines.length - 1)
+        .map((row) => row[i])
+        .join('')
+
+      if (+num) {
+        input.push(+num)
+      }
+    }
+    console.log({ operation, input, startIndex, endIndex })
+    lastIndex = endIndex + 1
+    problems.push({ operation, input })
+  }
+
+  let result = 0
+  for (const problem of problems) {
+    result += calcProblem(problem.input, problem.operation)
+  }
+  return result
+}
